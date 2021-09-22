7e9d1731191e566a9a99c3e9545ead369add21f9
John D Storey <john@nft.org>
2021-09-22 01:29:04 -0400

    adds input and button to mint page

--- packages/react-app/src/views/Mint.jsx
+++ packages/react-app/src/views/Mint.jsx
@@ -1,14 +1,19 @@
-import React from "react"
-export default (props) => {
-    return <div>
-        mint
-        {/* Start QA PROCESS */}
-        Do you know what kind of token you want to mint?
-            Yes:    COIN    - ERC20 w/o admin
-                    NFT     - ERC721 w/o admin
-                    MULTI   - ERC1155 w/o admin
-                    DAO     - CUSTOM
-            
-            No:     Contact form for consultation
+import React, { useState } from "react";
+import { Input, Button } from "semantic-ui-react";
+
+export default props => {
+  const [url, setUrl] = useState();
+  const handleClick = () => {
+    window.alert(`web3 tx here with ${url}`);
+  };
+  return (
+    <div className="flex justify-content-center mauto w300 mt20">
+      <div className=" flex column">
+        <Input className="mb10" onChange={e => setUrl(e.target.value)} placeholder="NFT URL" value={url} />
+        <Button primary onClick={handleClick}>
+          Deploy
+        </Button>
+      </div>
     </div>
-}
+  );
+};
