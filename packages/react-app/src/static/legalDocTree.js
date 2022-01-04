5d9e85100df635f761ae4d6ffb289bd591bb69c0
John D Storey <johnstorey@Johns-MacBook-Air.local>
2022-01-04 14:56:25 -0700

    prompts, roles & legal

---
+++ packages/react-app/src/static/legalDocTree.js
@@ -0,0 +1,22 @@
+export let array = [
+    {
+        name: "US trust",
+        shortDesc: "an agreement between two parties",
+        types: {}
+    },
+    {
+        name: "US trust",
+        shortDesc: "an agreement between two parties",
+        types: {}
+    },
+    {
+        name: "erc20dao",
+        shortDesc: "legally enabled governance token",
+    },
+    {
+        name: "nft",
+        shortDesc: "",
+        types: {}
+
+    }
+];
---
+++ packages/react-app/src/static/roles.js
@@ -0,0 +1,12 @@
+export const roles = [
+        {
+            name: "simple",
+            ubi: "true",
+            Tasks: "that can be completed by an individual online with almost no supervision or correction",
+        }
+    ]
+        
+    // craftsman:    UBI + 22           In person tasks, or need of knowledge with a specific tool, software, hardware, real world. Asthetic is important but not vital.
+    // experienced:  UBI + 33k             Aesthetic important with specic tool
+    // pivitol:      UBI + EQUITY + 50k  
+    // critical:     UBI + EQUITY + 100k 
