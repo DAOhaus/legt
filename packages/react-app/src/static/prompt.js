01ad99ff4ce48aa566e550ab4b0d41c0a809fd85
John D Storey <johnstorey@Johns-MacBook-Air.local>
2022-01-12 21:02:13 -0700

    adds formik & yup

--- packages/react-app/src/static/prompt.js
+++ packages/react-app/src/static/prompt.js
@@ -1,7 +1,8 @@
 // https://github.com/talor-hammond/formik-react-hooks-multi-step-form
 
-export const prompts = [
+export default [
     {
+        id: '1',
         category: 'general',
         text: 'What do you want to create a token for?',
         choices: ['business', 'personal residence', 'community project', 'other'],
@@ -14,26 +15,26 @@ export const prompts = [
     },
 ]
 
-business
-    - want it to be singly owned and operated?  This looks like an NFT
-    - would you like to find investors for your business?
-    - is it actively in operation and generating revenue?
-    - important to be SEC compliant?
+// business
+//     - want it to be singly owned and operated?  This looks like an NFT
+//     - would you like to find investors for your business?
+//     - is it actively in operation and generating revenue?
+//     - important to be SEC compliant?
 
-residence
-    - single or multi-family
-    - lease or own
-    - room in house
-    - residence under construction
-    - soon to be residence (mortgage)
+// residence
+//     - single or multi-family
+//     - lease or own
+//     - room in house
+//     - residence under construction
+//     - soon to be residence (mortgage)
 
-collective
-    - How many people
-    - What rights are important to share?
-        - right of use
-        - underlying equity in land
-        - underlying equity in business on land
-        - voting and governance
-    - Do you already have your legal framework established
-    - equal ownership spread out across entire group or specifically
+// collective
+//     - How many people
+//     - What rights are important to share?
+//         - right of use
+//         - underlying equity in land
+//         - underlying equity in business on land
+//         - voting and governance
+//     - Do you already have your legal framework established
+//     - equal ownership spread out across entire group or specifically
  
--- packages/react-app/src/views/Mint.jsx
+++ packages/react-app/src/views/Mint.jsx
@@ -1,5 +1,71 @@
 import React, { useState } from "react";
 import { Input, Button } from "semantic-ui-react";
+import prompt from '../static/prompt';
+import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage, useField, useFormikContext } from 'formik';
+
+const Wizard = ({ children, initialValues, onSubmit }) => {
+  const [stepNumber, setStepNumber] = useState(0);
+  const steps = React.Children.toArray(children);
+  const [snapshot, setSnapshot] = useState(initialValues);
+
+  const step = steps[stepNumber];
+  const totalSteps = steps.length;
+  const isLastStep = stepNumber === totalSteps - 1;
+
+  const next =values => {
+    setSnapshot(values);
+    setStepNumber(Math.min(stepNumber + 1, totalSteps - 1));
+  };
+
+  const previous = values => {
+    setSnapshot(values);
+    setStepNumber(Math.max(stepNumber - 1, 0));
+  };
+
+  const handleSubmit = async (values, bag) => {
+    if (step.props.onSubmit) {
+      await step.props.onSubmit(values, bag);
+    }
+    if (isLastStep) {
+      return onSubmit(values, bag);
+    } else {
+      bag.setTouched({});
+      next(values);
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={snapshot}
+      onSubmit={handleSubmit}
+      validationSchema={step.props.validationSchema}
+    >
+      {formik => (
+        <Form>
+          <p>
+            Step {stepNumber + 1} of {totalSteps}
+          </p>
+          {step}
+          <div style={{ display: 'flex' }}>
+            {stepNumber > 0 && (
+              <button onClick={() => previous(formik.values)} type="button">
+                Back
+              </button>
+            )}
+            <div>
+              <button disabled={formik.isSubmitting} type="submit">
+                {isLastStep ? 'Submit' : 'Next'}
+              </button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+const WizardStep = ({ children }) => children;
 
 export default props => {
   const [url, setUrl] = useState();
@@ -13,6 +79,70 @@ export default props => {
         <Button primary onClick={handleClick}>
           Deploy
         </Button>
+        <Wizard
+      initialValues={{
+        email: '',
+        firstName: '',
+        lastName: '',
+      }}
+      onSubmit={async values =>
+        sleep(300).then(() => console.log('Wizard submit', values))
+      }
+    >
+      <WizardStep
+        onSubmit={() => console.log('Step1 onSubmit')}
+        validationSchema={Yup.object({
+          firstName: Yup.string().required('required'),
+          lastName: Yup.string().required('required'),
+        })}
+      >
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <Field
+            autoComplete="given-name"
+            component="input"
+            id="firstName"
+            name="firstName"
+            placeholder="First Name"
+            type="text"
+          />
+          <ErrorMessage className="error" component="div" name="firstName" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <Field
+            autoComplete="family-name"
+            component="input"
+            id="lastName"
+            name="lastName"
+            placeholder="Last Name"
+            type="text"
+          />
+          <ErrorMessage className="error" component="div" name="lastName" />
+        </div>
+      </WizardStep>
+      <WizardStep
+        onSubmit={() => console.log('Step2 onSubmit')}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('required'),
+        })}
+      >
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field
+            autoComplete="email"
+            component="input"
+            id="email"
+            name="email"
+            placeholder="Email"
+            type="text"
+          />
+          <ErrorMessage className="error" component="div" name="email" />
+        </div>
+      </WizardStep>
+    </Wizard>
       </div>
     </div>
   );
