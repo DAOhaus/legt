70a0a80eb83823a5135c42d9442aaf9d0b60c73d
j0xhn <2646310+j0xhn@users.noreply.github.com>
2023-01-03 01:36:14 -0700

    working goerli liquidity page

--- src/views/Liquidity.js
+++ src/views/Liquidity.js
@@ -1,28 +1,59 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { ethers } from "ethers";
-import { uniswapFactory, goerli } from '../helpers/addresses.json'
+import addresses from '../helpers/addresses.json'
+// import erc20abi from '../helpers/erc20abi.js'
 import { useParams } from "react-router-dom";
+import { Card } from 'antd';
 import { getPriceUniswapV3, queryFactoryForLPUniV3 } from '@thanpolas/uniswap-chain-queries'
+const erc20abi = [
+    // Read-Only Functions
+    "function balanceOf(address owner) view returns (uint256)",
+    "function decimals() view returns (uint8)",
+    "function symbol() view returns (string)",
+
+    // Authenticated Functions
+    "function transfer(address to, uint amount) returns (bool)",
+
+    // Events
+    "event Transfer(address indexed from, address indexed to, uint amount)"
+];
 
 function Liquidity() {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const network = 'goerli'
     let { addressParam } = useParams();
+    const [data, setData] = useState([]);
+    const [tokenInfo, setTokenInfo] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const provider = ethers.getDefaultProvider(network, { alchemy: process.env.REACT_APP_ALCHEMY_KEY_GOERLI })
+    const stable = addresses[network].stable
+    const StableContract = new ethers.Contract( stable, erc20abi, provider )
+    const AssetContract = new ethers.Contract( addressParam, erc20abi, provider )
+    const StableSymbol = StableContract.symbol()
+    const AssetSymbol = AssetContract.symbol()
+    const getTokenInfo= useCallback(async () => {
+        const stableSymbol = await StableContract.symbol()
+        const assetSymbol = await AssetContract.symbol()
+        console.log('symbols', StableSymbol, AssetSymbol)
+        return [stableSymbol, assetSymbol]
+    })
     const getData = useCallback(async () => {
-        const provider = ethers.getDefaultProvider('goerli', { alchemy: process.env.REACT_APP_ALCHEMY_KEY_GOERLI })
         const lpAddresses = await queryFactoryForLPUniV3(
-            uniswapFactory,
+            addresses.uniswapFactory,
             provider,
-            [goerli.stable, addressParam],
+            [stable, addressParam],
         );
-        const newData = await Promise.all(lpAddresses.map(async (address) => getPriceUniswapV3(address, provider, [18, 18])))
+        const newData = await Promise.all(lpAddresses.map(async (address) => 
+            getPriceUniswapV3(address, provider, [18, 18])
+        ))
         setLoading(false)
         return newData
-    }, [addressParam])
+    }, [stable,addressParam])
 
     useEffect(() => {
         setLoading(true)
         async function dataFetch() { setData(await getData()) }
+        async function tokenFetch() { setTokenInfo(await getTokenInfo()) }
+        tokenFetch()
         dataFetch()
     }, [getData]);
 
@@ -31,12 +62,26 @@ function Liquidity() {
             {loading
                 ? 'loading...'
                 : <div className="m20">
-                    Goerli Liquidity Pools:
                     <div className="m20" />
-                    <div className='flex word-break'>{data.map(asset =>
-                        <div key={asset.lpAddress}>{JSON.stringify(asset, null, 4)}</div>
+                    <div className='flex word-break'>{data
+                    .filter(asset => Number(asset.token0Reserves) || Number(asset.token1Reserves))
+                    .map(asset =>
+                    <div key={asset.lpAddresses} >
+                            <Card title={`${network} uniswap`} >
+                                
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href={`https://app.uniswap.org/#/swap?inputCurrency=${stable}&outputCurrency=${addressParam}`}>
+                                    <button>{`${tokenInfo[0]} / ${tokenInfo[1]} ⇗`}</button>
+                                </a>
+                                {Object.entries(asset).map(([key, value]) =>
+                                    <p>{`${key}: ${value}`}</p>
+                                )}
+                            </Card>
+                        </div>
                     )}</div>
-                    <button onClick={async () => setData(await getData())}>refresh data</button>
+                    {/* <button onClick={async () => setData(await getData())}>refresh data</button> */}
                 </div>}
 
         </div>
